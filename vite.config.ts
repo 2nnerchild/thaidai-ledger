1bc49dfc78fcaeb7f9bfbd7f936b3af20b30f04f
박성국 <thaidai@example.com>
2026-05-18 16:28:44 +0700

    fix: vite config TypeScript 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -49,11 +48,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
-  },
-  test: {
-    globals: true,
-    environment: 'node',
   },
 });
